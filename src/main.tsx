0c9597181028c9901e11f552888f5bdd6ef24812
Jiawei Zhu <allenzhu724@gmail.com>
2024-12-15 20:29:10 -0500

    Fixed bug rendering

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from './App';
-import './styles/index.css';
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import { App } from './App'
+import './styles/index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const rootElement = document.getElementById('root')
+if (!rootElement) throw new Error('Failed to find the root element')
+
+const root = createRoot(rootElement)
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+)
